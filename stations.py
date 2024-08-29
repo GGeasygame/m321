eb209d71b7a8b78cae63d0efbce3057372899dae
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2024-08-29 13:53:00 +0200

    stations.py geupdated

--- stations.py
+++ stations.py
@@ -6,3 +6,4 @@ class Station(Enum):
     VESTA_STATION = {"x": 10000, "y": 10000, "name": "Vesta Station"}
     CORE_STATION = {"x": 0, "y": 0, "name": "Core Station"}
     AZURA_STATION = {"x": -1000, "y": 1000, "name": "Azura Station"}
+    STATION_19_A = {"x": -15526, "y": -12527, "name": "Station 19-A"}
