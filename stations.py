e86013ec716a67b2739a539016fc24106c2fd39a
bjorngedeon <bjoern.gedeon@gmail.com>
2024-09-05 14:04:01 +0200

    update stations

--- stations.py
+++ stations.py
@@ -7,7 +7,7 @@ class Station(Enum):
     CORE_STATION = {"x": 0, "y": 0, "name": "Core Station"}
     AZURA_STATION = {"x": -1000, "y": 1000, "name": "Azura Station"}
     STATION_19_A = {"x": -15526, "y": -12527, "name": "Station 19-A"}
+    STATION_19_B = {"x": -6074, "y": 18868, "name": "Station 19-B"}
     SHANGRIS_STATION = {"x": 4143, "y": 4808, "name": "Shangris Station"}
     GOLD_STONE = {"x": -10200, "y": 20700, "name": "Gold Stone"}
     ARAK_STATION = {"x": 2712, "y": -4044, "name": "Arak Stone"}
-    A19 = {"x": -15526, "y": -12527, "name": "A-19"}
