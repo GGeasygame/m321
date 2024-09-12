381dda2566cce15106776c243c7bbd018797c75b
bjorngedeon <bjoern.gedeon@gmail.com>
2024-09-12 12:57:56 +0200

    commit msg

--- interfaces/laser.py
+++ interfaces/laser.py
@@ -26,3 +26,19 @@ def get_state():
     response = requests.get(url)
 
     print(response.json())
+
+
+def setup_keycloak():
+    try:
+        data = {"client_secret": "79kdqCGiq6aocxVcP3oiGhDTUFU3nu5W",
+                "authorize_url": "http://192.168.100.19:8080/realms/master/protocol/openid-connect/auth",
+                "token_url": "http://192.168.100.19:8080/realms/master/protocol/openid-connect/token"}
+
+        response = requests.post("http://192.168.100.19:2018/configure_oauth", json=data)
+        return response.status_code, response.text
+    except requests.exceptions.RequestException as e:
+        return None, str(e)
+
+
+print(setup_keycloak())
+
--- stations.py
+++ stations.py
@@ -8,6 +8,6 @@ class Station(Enum):
     AZURA_STATION = {"x": -1000, "y": 1000, "name": "Azura Station"}
     STATION_19_A = {"x": -15526, "y": -12527, "name": "Station 19-A"}
     STATION_19_B = {"x": -6074, "y": 18868, "name": "Station 19-B"}
-    SHANGRIS_STATION = {"x": 4143, "y": 4808, "name": "Shangris Station"}
+    SHANGRIS_STATION = {"x": 4639, "y": -4919, "name": "Shangris Station"}
     GOLD_STONE = {"x": -10200, "y": 20700, "name": "Gold Stone"}
     ARAK_STATION = {"x": 2712, "y": -4044, "name": "Arak Stone"}
